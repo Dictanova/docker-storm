05bb5d4cb55bcec138702cee17aab7ca8954da41
Damien Raude-Morvan <drazzib@drazzib.com>
2016-02-19 22:16:57 +0100

    Import 0.9.3

---
+++ storm/start-supervisor.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" ${STORM_HOME}/conf/storm.yaml
+sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" ${STORM_HOME}/conf/storm.yaml
+sed -i -e "s/%drpc%/$DRPC_PORT_3772_TCP_ADDR/g" ${STORM_HOME}/conf/storm.yaml
+
+echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+
+supervisord
